e7b2e509b2847b763043577f27cc2544a5ae035c
Jim Chemmalakuzh <chemmalakuzhyj@gmail.com>
2017-04-19 01:02:16 -0400

    Created Controller Class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,16 +5,73 @@
  */
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
 /**
+ * FXML Controller class
  *
  * @author chemm
  */
-public class Controller {
-    
-    //add
-    
-    //delete
-    
-    
+public class Controller implements Initializable {
+
+    @FXML
+    private Pane pokemon_1;
+    @FXML
+    private Label pokemon_1_name;
+    @FXML
+    private Label pokemon_1_type1;
+    @FXML
+    private Pane pokemon_2;
+    @FXML
+    private Label pokemon_2_name;
+    @FXML
+    private Label pokemon_2_type1;
+    @FXML
+    private Label pokemon_2_type2;
+    @FXML
+    private Pane pokemon_3;
+    @FXML
+    private Label pokemon_3_name;
+    @FXML
+    private Label pokemon_3_type1;
+    @FXML
+    private Label pokemon_3_type2;
+    @FXML
+    private Pane pokemon_4;
+    @FXML
+    private Label pokemon_4_name;
+    @FXML
+    private Label pokemon_4_type1;
+    @FXML
+    private Label pokemon_4_type2;
+    @FXML
+    private Pane pokemon_5;
+    @FXML
+    private Label pokemon_5_name;
+    @FXML
+    private Label pokemon_5_type1;
+    @FXML
+    private Label pokemon_5_type2;
+    @FXML
+    private Pane pokemon_6;
+    @FXML
+    private Label pokemon_6_name;
+    @FXML
+    private Label pokemon_6_type1;
+    @FXML
+    private Label pokemon_6_type2;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
     
 }
