aaf840044278251c119c81a9fa02ab65ab14d629
vthomas13 <vthomas13@gmail.com>
2017-04-19 02:25:28 -0400

    modded dbutil, connected to db

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -5,25 +5,38 @@
  */
 package util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  *
  * @author chemm
  */
 public class DBUtil {
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/pokemon_team_builder";
+   static final String USER = "root";
+   static final String PASS = "root@localhost";
     private DBUtil(){
         
     }
     
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
         Connection connection = null;
-        
-        String projectDirectory = System.getProperty("user.dir");
-        connection = DriverManager.getConnection("jdbc:mysql: " + projectDirectory +"/pokemon_team_builder","root","Luminis1992");
+        Statement stmt = null;
+        try{
+        //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+        //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+        }catch(SQLException se){
+        //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+        //Handle errors for Class.forName
+            e.printStackTrace();
+        }
         return connection;
     }
 }
